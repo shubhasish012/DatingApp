6af63569d65fb9786e5b3d46a7487ff3262e3381
nissan <shubhasish012@gmail.com>
2020-11-30 17:24:01 +0600

    parent child data passing

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,9 +11,9 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
     model: any = {};
-    loggedIn:boolean;
 
-    constructor(private accountService: AccountService) { }
+
+    constructor(public accountService: AccountService) { }
 
     ngOnInit(): void {
     }
@@ -19,10 +21,14 @@ export class NavComponent implements OnInit {
     login() {
         this.accountService.login(this.model).subscribe((response) => {
             console.log(response);
-            this.loggedIn=true;
         }, error => {
             console.log(error);
         })
     }
+
+    logout() {
+        this.accountService.logout();
+    }
+
 
 }
