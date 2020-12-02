fb1ffe551b43fe0e9abea901bf3f8486c3931da9
nissan <shubhasish012@gmail.com>
2020-12-02 16:13:37 +0600

    Add seed data for api

--- API/DatingApp/Entities/AppUser.cs
+++ API/DatingApp/Entities/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DatingApp.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,5 +15,32 @@ namespace DatingApp.Entities
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
