b178f935645d0f1872839a0d294dcfd5a45f5a11
nissan <shubhasish012@gmail.com>
2020-12-14 14:26:58 +0600

    add pagination api

--- API/DatingApp/Helpers/UserParams.cs
+++ API/DatingApp/Helpers/UserParams.cs
@@ -7,5 +7,10 @@ namespace DatingApp.Helpers
 {
     public class UserParams
     {
+        private const int MaxPageSize = 50;
+
+        public int PageNumber { get; set; } = 1;
+
+        public int _pageSize = 10;
     }
 }
