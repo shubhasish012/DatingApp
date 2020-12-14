1bfbbbc3155941550035b30af82ac05f040a8cab
nissan <shubhasish012@gmail.com>
2020-12-14 23:17:57 +0600

    userParams helper class

--- API/DatingApp/Helpers/UserParams.cs
+++ API/DatingApp/Helpers/UserParams.cs
@@ -12,5 +12,11 @@ namespace DatingApp.Helpers
         public int PageNumber { get; set; } = 1;
 
         public int _pageSize = 10;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
     }
 }
