b7eb043a3f22e018695014e46d977a29a52420a4
nissan <shubhasish012@gmail.com>
2021-01-30 21:59:58 +0600

    fix changes

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -15,6 +15,8 @@ export class MemberMessagesComponent implements OnInit {
   @Input() messages: Message[];
   @Input() username: string;
   messageContent: string;
+  loading = false;
+
   constructor(public messageService:MessageService) { }
 
   ngOnInit(): void {
@@ -23,7 +25,7 @@ export class MemberMessagesComponent implements OnInit {
   sendMessage(){
     this.messageService.sendMessage(this.username,this.messageContent).then(()=>{
       this.messageForm.reset();
-    })
+    }).finally(()=>this.loading =false);
   }
 
 
